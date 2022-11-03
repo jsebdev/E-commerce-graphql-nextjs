11fb3e19ab098fe6e78dc8c0d291b58c04d25e17
Sebastian Caicedo <jsebdev@gmail.com>
2022-11-03 13:04:07 +0100

    Wrap login form

---
+++ fronend/components/loginForm.js
@@ -0,0 +1,43 @@
+import { useLogin } from "hooksAndLogic/login.hook";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import React from "react";
+
+export const LoginForm = ({ setErrorMessages }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { handleLogin, formSettings, handleFormErrors } = useLogin(
+    dispatch,
+    router
+  );
+  const form = useForm(formSettings);
+  const login = async (e) => {
+    const { success, errors } = await handleLogin(e);
+    if (!success) {
+      setErrorMessages(errors?.nonFieldErrors?.map((error) => error.message));
+    }
+  };
+
+  return (
+    <>
+      <h3>Log in</h3>
+      <form onSubmit={form.onSubmit(login, handleFormErrors)}>
+        <TextInput
+          id="username"
+          placeholder="username"
+          label="username"
+          {...form.getInputProps("username")}
+        />
+        <PasswordInput
+          id="password"
+          placeholder="password"
+          label="password"
+          {...form.getInputProps("password")}
+        />
+        <Button type="submit">Log In</Button>
+      </form>
+    </>
+  );
+};
--- fronend/pages/login.js
+++ fronend/pages/login.js
@@ -1,65 +1,9 @@
-import { Button, PasswordInput, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { DynamicUserChecker } from "components/dynamicUseChecker";
-import { Layout } from "components/layout";
-import { useLogin } from "hooksAndLogic/login.hook";
-import { useProfile } from "hooksAndLogic/profile.hook";
-import { useRouter } from "next/router";
 import React from "react";
-import { connect, useDispatch } from "react-redux";
-import { selectToken, selectUsername } from "store/slices/userSlice";
+import { AuthFormWrapperNoConnection } from "components/authFormWrapper";
+import { LoginForm } from "components/loginForm";
 
-const login = ({ token, username }) => {
-  const [errorMessages, setErrorMessages] = React.useState([]);
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const { checkNoUser } = useProfile({ token, username, router });
-  checkNoUser();
-  const { handleLogin, formSettings, handleFormErrors } = useLogin(
-    dispatch,
-    router
-  );
-  const form = useForm(formSettings);
-  const login = async (e) => {
-    const { success, errors } = await handleLogin(e);
-    if (!success) {
-      setErrorMessages(errors?.nonFieldErrors?.map((error) => error.message));
-    }
-  };
-
-  return (
-    <Layout home={false}>
-      <DynamicUserChecker condition={!token || !username}>
-        <h3>Log in</h3>
-        <form onSubmit={form.onSubmit(login, handleFormErrors)}>
-          <TextInput
-            id="username"
-            placeholder="username"
-            label="username"
-            {...form.getInputProps("username")}
-          />
-          <PasswordInput
-            id="password"
-            placeholder="password"
-            label="password"
-            {...form.getInputProps("password")}
-          />
-          <Button type="submit">Log In</Button>
-        </form>
-        {errorMessages.length > 0 && (
-          <>
-            <p>Errors:</p>
-            {errorMessages.map((message) => (
-              <p key={message}>{message}</p>
-            ))}
-          </>
-        )}
-      </DynamicUserChecker>
-    </Layout>
-  );
+const SignUp = () => {
+  return <AuthFormWrapperNoConnection FormComponent={LoginForm} />;
 };
 
-export default connect((state) => ({
-  token: selectToken(state),
-  username: selectUsername(state),
-}))(login);
+export default SignUp;
