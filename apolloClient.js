9fff2134afc0178371e06862d95d005aba157fa3
Sebastian Caicedo <jsebdev@gmail.com>
2022-11-19 23:12:49 +0100

    Change apollo uri depending on environment

--- apolloClient.js
+++ apolloClient.js
@@ -1,9 +1,15 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
+console.log(
+  "4: process.env.BACKEND_URI >>>",
+  process.env.NEXT_PUBLIC_BACKEND_URI
+);
+
 export const client = new ApolloClient({
   link: createUploadLink({
-    uri: "http://localhost:8000/graphql/",
+    uri: process.env.NEXT_PUBLIC_BACKEND_URI,
+    // uri: "http://localhost:8001/graphql/",
   }),
   cache: new InMemoryCache(),
 });
