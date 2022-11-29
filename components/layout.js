281f825557c3bd4493d720b1dfbac5ac32c5dd24
Sebastian Caicedo <jsebdev@gmail.com>
2022-11-29 01:09:44 +0100

    Change og:image tag

--- components/layout.js
+++ components/layout.js
@@ -5,16 +5,17 @@ import { Header } from "./header";
 import { Footer } from "./footer";
 import { Container } from "@mantine/core";
 
-export const siteTitle = "Awesome Port-Ecommerce";
-
 export const Layout = ({ children }) => {
   return (
     <div className={styles.containerLayout}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Awesome E-commerce website" />
-        <meta property="og:image" content="/favicon.ico" />
-        <meta name="og:title" content={siteTitle} />
+        <meta
+          property="og:image"
+          content="https://jseb.dev/static/media/ecommerce.1a42b43ab95ed22bf8bf.png"
+        />
+        <meta name="og:title" content="Awesome Port-Ecommerce" />
       </Head>
       <Header />
       <main className={styles.main}>
